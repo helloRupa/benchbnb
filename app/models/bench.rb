e63c1f8f24803ae1bffaec564d2c728f14ca90fa
Rupa Dhillon <rupa211984@gmail.com>
2019-07-25 21:35:10 -0700

    Only add in bounds benches to index

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -1,4 +1,17 @@
 class Bench < ApplicationRecord
   validates :description, :lat, :lng, presence: true
   validates :lat, :lng, numericality: true
+
+  def self.in_bounds(bounds)
+    lats = [bounds['northEast']['lat'], bounds['southWest']['lat']].map(&:to_f).sort
+    lngs = [bounds['northEast']['lng'], bounds['southWest']['lng']].map(&:to_f).sort
+
+    Bench.all.reduce([]) do |benches, bench|
+      if bench['lat'].between?(lats[0], lats[1]) && bench['lng'].between?(lngs[0], lngs[1])
+        benches << bench
+      end
+
+      benches
+    end
+  end
 end
